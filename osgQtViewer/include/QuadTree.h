ccf5905919b3fcb4785d9aa8c082983f7f5146e5
Jack721nju <Wuyang_nju@126.com>
2022-05-11 23:55:10 +0800

    add quad tree

---
+++ osgQtViewer/include/QuadTree.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QPoint>
+#include <vector>
+
+#include "struct.h"
+
+//定义八叉树节点类
+class QuadTreeNode {
+public:
+	QuadTreeNode(
+		int depth,
+		point2D_MAXMIN curSize,
+		QuadTreeNode * top_left_Node = nullptr,
+		QuadTreeNode * top_right_Node = nullptr,
+		QuadTreeNode * bottom_left_Node = nullptr,
+		QuadTreeNode * bottom_right_Node = nullptr
+	) : m_depth(depth), m_XY_Size(curSize),
+		m_top_left(top_left_Node), m_top_right(top_right_Node), m_bottom_left(bottom_left_Node), m_bottom_right(bottom_right_Node), m_point_num(0) {}
+
+	~QuadTreeNode() = default;
+
+private:
+	//节点深度
+	int m_depth;
+
+	//节点坐标
+	point2D_MAXMIN m_XY_Size;
+
+	int m_tree_Index;
+	int m_point_num;
+
+	std::vector<QPoint> point_list;
+
+	QuadTreeNode * m_top_left;
+	QuadTreeNode * m_top_right;
+	QuadTreeNode * m_bottom_left;
+	QuadTreeNode * m_bottom_right;
+
+public:
+	void createQuadTree(QuadTreeNode* rootNode, int treeDepth, const std::vector<QPoint> &point_list, const point2D_MAXMIN &curSize);
+
+};

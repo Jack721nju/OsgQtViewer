81d0ebeec6825ac5c2056ab4d0387be1f5c5592e
Jack721nju <Wuyang_nju@126.com>
2022-05-29 23:17:10 +0800

    update 0529

--- osgQtViewer/include/TimerClock.h
+++ osgQtViewer/include/TimerClock.h
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <chrono>
 
-typedef std::chrono::minutes       Min;
-typedef std::chrono::seconds       Sec;
-typedef std::chrono::milliseconds  Ms;
-typedef std::chrono::microseconds  Us;
+using Min = std::chrono::minutes;
+using Sec = std::chrono::seconds;
+using Ms = std::chrono::milliseconds;
+using Us = std::chrono::microseconds;
+
+using SteadyClock = std::chrono::steady_clock;
 
 class TimerClock {
  public:
@@ -16,17 +18,17 @@ class TimerClock {
 
 	// 开始or重新计时
 	void start() {
-		_start = std::chrono::steady_clock::now();
+		_start = SteadyClock::now();
 	}
 
 	// 获取时间，根据模板设置时间单位
 	template <typename clockUnit = Sec>
 	auto getTime() {
-		_end = std::chrono::steady_clock::now();
+		_end = SteadyClock::now();
 		return std::chrono::duration_cast<clockUnit>(_end - _start).count();
 	}
 
  private:
-	std::chrono::time_point<std::chrono::steady_clock> _start;
-	std::chrono::time_point<std::chrono::steady_clock> _end;
+	std::chrono::time_point<SteadyClock> _start;
+	std::chrono::time_point<SteadyClock> _end;
 };
